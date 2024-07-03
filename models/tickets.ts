c0dec417e148a694cd978e666d3a37493a6d433d
FrancescoParisi02 <francescoparisi178@gmail.com>
2024-07-03 16:06:20 +0200

    formatting the code

--- models/tickets.ts
+++ models/tickets.ts
@@ -15,11 +15,11 @@ const sequelize: Sequelize = DBIsConnected.getInstance();
  */
 export const Ticket = sequelize.define('tickets', {
     id_ticket: { type: DataTypes.STRING, allowNull: false, primaryKey: true },
-    weather: { type: DataTypes.ENUM('good weather, bad weather'), allowNull: false }, // true means good conditions
+    weather: { type: DataTypes.ENUM('good weather, bad weather'), allowNull: false }, //we choose to work with only two generic weather conditions
     plate: { type: DataTypes.STRING, allowNull: false },
     ticket_date: { type: DataTypes.DATE, allowNull: false },
-    initial_gate: { type: DataTypes.STRING, allowNull: false, references:{model: Gate, key: 'location'} },
-    final_gate: { type: DataTypes.STRING, allowNull: false, references:{model: Gate, key: 'location'} },
+    initial_gate: { type: DataTypes.STRING, allowNull: false, references: { model: Gate, key: 'location' } },
+    final_gate: { type: DataTypes.STRING, allowNull: false, references: { model: Gate, key: 'location' } },
     medium_speed: { type: DataTypes.FLOAT, allowNull: false },
     delta_limit: { type: DataTypes.FLOAT, allowNull: false },
 },
