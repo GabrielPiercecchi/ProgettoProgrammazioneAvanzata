99f1dd9c72edaf6babe03922889ca0123d4ef3c4
GabrielP <S1097612@studenti.univpm.it>
2024-07-03 17:46:36 +0200

    Models Recap before the implementation of the controllers

--- models/operator.ts
+++ models/operator.ts
@@ -1,8 +1,12 @@
 import { DBIsConnected } from "../database/database";
 import { DataTypes, Sequelize, Model } from 'sequelize';
 import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
 
 dotenv.config();
+
+const SALT_ROUNDS = 10;
+
 //Connection to DataBase
 const sequelize: Sequelize = DBIsConnected.getInstance();
 
@@ -12,9 +16,19 @@ const sequelize: Sequelize = DBIsConnected.getInstance();
  * Define the model 'Operator'
  */
 export const Operator = sequelize.define('operator', {
-    id_operator: { type: DataTypes.STRING, defaultValue: process.env.DEFAULT_ID_OPERATOR, primaryKey: true },
-    username: { type: DataTypes.STRING, defaultValue: process.env.DEFAULT_USERNAME_OPERATOR, allowNull: false, unique: true }, //TODO:regex
-    password: { type: DataTypes.STRING, defaultValue: process.env.DEFAULT_PASSWORD_OPERATOR, allowNull: false }, //TODO:regex
+    id_operator: { 
+        type: DataTypes.STRING, 
+        defaultValue: process.env.DEFAULT_ID_OPERATOR, 
+        primaryKey: true },
+    username: { 
+        type: DataTypes.STRING,
+        defaultValue: process.env.DEFAULT_USERNAME_OPERATOR, 
+        allowNull: false, 
+        unique: true }, //TODO:regex
+    password: { 
+        type: DataTypes.STRING, 
+        defaultValue: process.env.DEFAULT_PASSWORD_OPERATOR, 
+        allowNull: false }, //TODO:regex
 },
     {
         modelName: 'operator',
@@ -25,9 +39,12 @@ export const Operator = sequelize.define('operator', {
          * operator and checks if the id_operator is equal to is default value
          */
         hooks: {
-            beforeCreate: (operator: Model<any, any>, options) => {
-                if (operator.getDataValue('id_operator') !== 'operator') {
-                    throw new Error('Id_operator cant be modified.');
+            beforeCreate: async (operator, options) => {
+                if (operator.getDataValue('id_operator') !== process.env.DEFAULT_ID_OPERATOR) {
+                    throw new Error('Id_operator cannot be changed.');
+                }
+                if ((operator as any).changed('password')) {
+                    (operator as any).password = await bcrypt.hash((operator as any).password, SALT_ROUNDS);
                 }
             },
         },
